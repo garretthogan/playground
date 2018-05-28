195ec69383d37e5f37fc0c627ae740b4927b907b
Garrett Hogan <garrett.hogan.dev@gmail.com>
2018-05-27 17:02:00 -0700

    init

--- controllers/api.js
+++ controllers/api.js
@@ -408,7 +408,7 @@ exports.postTwilio = (req, res, next) => {
 
   const message = {
     to: req.body.number,
-    from: '+13472235148',
+    from: '+12252636117',
     body: req.body.message
   };
   twilio.messages.create(message).then((sentMessage) => {
